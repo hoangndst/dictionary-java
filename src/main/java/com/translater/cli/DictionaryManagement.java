c078d1413c83171a7d29cff9262b504ac6459ffb
hoangndst <hoangndst@gmail.com>
2021-09-21 20:02:45 +0700

    from hoangndst

--- src/main/java/com/translater/cli/DictionaryManagement.java
+++ src/main/java/com/translater/cli/DictionaryManagement.java
@@ -1,5 +1,7 @@
 package com.translater.cli;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class DictionaryManagement {
@@ -18,7 +20,15 @@ public class DictionaryManagement {
         scanner.close();
     }
 
-    public void insertFromFile(String path) {
-        
+    public void insertFromFile(String path) throws FileNotFoundException {
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String wordSource = scanner.nextLine();
+            String wordTarget = scanner.nextLine();
+            Word newWord = new Word(wordSource.toLowerCase(), wordTarget);
+            dictionary.insert(newWord);
+        }
+        scanner.close();
     }
 }
