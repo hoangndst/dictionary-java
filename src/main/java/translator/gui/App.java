f87d205d9f5f0ade4b6cbdeac7a559d1c31ac4d2
Hoang Nguyen Dinh <hoangndst>
2021-10-29 13:45:32 +0700

    last commit

--- src/main/java/translator/gui/App.java
+++ src/main/java/translator/gui/App.java
@@ -1,5 +1,7 @@
 package translator.gui;
 
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,21 +9,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-  public static void main(String[] args) {
-    launch(args);
-  }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    // FXMLLoader loader = new FXMLLoader(new
-    // File("C:/Users/hoang/Desktop/code/dictionary-java/src/main/resources/fxml/demo.fxml").toURI().toURL());
-    // Parent root = loader.load();
-    Parent root = FXMLLoader.load(getClass().getResource("../../fxml/demo.fxml"));
-    primaryStage.setTitle("Translator");
-    primaryStage.getIcons()
-        .add(new javafx.scene.image.Image("file:src/main/resources/assert/icon.png"));
-    primaryStage.setScene(new Scene(root, 800, 500));
-    primaryStage.resizableProperty().setValue(false);
-    primaryStage.show();
-  }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// FXMLLoader loader = new FXMLLoader(new File("C:/Users/hoang/Desktop/OOP/dictionary-java/src/main/resources/fxml/demo.fxml").toURI().toURL());
+		// Parent root = loader.load();
+		Parent root = FXMLLoader.load(getClass().getResource("../../fxml/demo.fxml"));
+		primaryStage.setTitle("Translator");
+		primaryStage.getIcons()
+			.add(new javafx.scene.image.Image("file:src/main/resources/assert/icon.png"));
+		primaryStage.setScene(new Scene(root, 800, 500));
+		primaryStage.resizableProperty().setValue(false);
+		primaryStage.show();
+	}
 }
--- src/main/java/translator/gui/controllers/DashboardController.java
+++ src/main/java/translator/gui/controllers/DashboardController.java
@@ -1,5 +1,6 @@
 package translator.gui.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
@@ -30,322 +31,321 @@ import translator.api.Translate;
 
 public class DashboardController implements Initializable {
 
-  private Word word;
-  private String sourceLang;
-  private String targetLang;
+	private Word word;
+	private String sourceLang;
+	private String targetLang;
+	
+	private Stack<Word> history = new Stack<>();
+	private Stack<Word> future = new Stack<>();
 
-  private Stack<Word> history = new Stack<>();
-  private Stack<Word> future = new Stack<>();
+  	private final HashMap<String, String> languages = new HashMap<String, String>() {
+    	{
+      		put("English", "en");
+      		put("French", "fr");
+      		put("German", "de");
+			put("Italian", "it");
+			put("Spanish", "es");
+			put("Vietnamese", "vi");
+			put("Chinese (Simplified)", "zh-CN");
+			put("Chinese (Traditional)", "zh-TW");
+			put("Japanese", "ja");
+			put("Korean", "ko");
+			put("Portuguese", "pt");
+			put("Russian", "ru");
+			put("Turkish", "tr");
+			put("Thai", "th");
+			put("Lao", "lo");
+    	}
+  	};
 
-  private final HashMap<String, String> languages = new HashMap<String, String>() {
-    {
-      put("English", "en");
-      put("French", "fr");
-      put("German", "de");
-      put("Italian", "it");
-      put("Spanish", "es");
-      put("Vietnamese", "vi");
-      put("Chinese (Simplified)", "zh-CN");
-      put("Chinese (Traditional)", "zh-TW");
-      put("Japanese", "ja");
-      put("Korean", "ko");
-      put("Portuguese", "pt");
-      put("Russian", "ru");
-      put("Turkish", "tr");
-      put("Thai", "th");
-      put("Lao", "lo");
-    }
-  };
+  	public void setWord(Word word) {
+    	this.word = word;
+  	}
 
-  public void setWord(Word word) {
-    this.word = word;
-  }
+	@FXML
+	private ResourceBundle resources;
 
-  @FXML
-  private ResourceBundle resources;
+	@FXML
+	private URL location;
 
-  @FXML
-  private URL location;
+	// TextArea
 
-  // TextArea
+	@FXML
+	private JFXTextArea InputTextField;
 
-  @FXML
-  private JFXTextArea InputTextField;
+	@FXML
+	private JFXTextArea OutputTextArea;
 
-  @FXML
-  private JFXTextArea OutputTextArea;
+	@FXML
+	private JFXTextArea stringTextArea;
 
-  @FXML
-  private JFXTextArea stringTextArea;
+	@FXML
+	private JFXTextArea longOutputTextArea;
 
-  @FXML
-  private JFXTextArea longOutputTextArea;
+	//
 
-  //
+	@FXML
+	private JFXButton audioButton;
 
-  @FXML
-  private JFXButton audioButton;
+	@FXML
+	private JFXButton copySourceButton;
 
-  @FXML
-  private JFXButton copySourceButton;
+	@FXML
+	private JFXButton copyTargetButton;
 
-  @FXML
-  private JFXButton copyTargetButton;
+	@FXML
+	private JFXButton redoButton;
 
-  @FXML
-  private JFXButton redoButton;
+	@FXML
+	private JFXButton aboutButton;
 
-  @FXML
-  private JFXButton aboutButton;
+	@FXML
+	private JFXButton translateButton;
 
-  @FXML
-  private JFXButton translateButton;
+	@FXML
+	private JFXButton bookMarkButton;
 
-  @FXML
-  private JFXButton bookMarkButton;
+	@FXML
+	private JFXButton showBMButton;
 
-  @FXML
-  private JFXButton showBMButton;
+	@FXML
+	private JFXComboBox<String> selectSourceBox;
 
-  @FXML
-  private JFXComboBox<String> selectSourceBox;
+	@FXML
+	private JFXComboBox<String> selectTargetBox;
 
-  @FXML
-  private JFXComboBox<String> selectTargetBox;
+	@FXML
+	private JFXButton swapButton;
 
-  @FXML
-  private JFXButton swapButton;
+	@FXML
+	private JFXListView<?> textListViewBox;
 
-  @FXML
-  private JFXListView<?> textListViewBox;
-
-  @FXML
-  private JFXButton undoButton;
-
-  @FXML
-  void audio(ActionEvent event) {
-    String URL = "https:" + this.word.getAudio();
-    Media sound = new Media(URL);
-    MediaPlayer mediaPlayer = new MediaPlayer(sound);
-    mediaPlayer.play();
-  }
+	@FXML
+	private JFXButton undoButton;
 
   @FXML
-  void copySource(ActionEvent event) {
-    final Clipboard clipboard = Clipboard.getSystemClipboard();
-    final ClipboardContent content = new ClipboardContent();
-    content.putString(InputTextField.getText());
-    clipboard.setContent(content);
-  }
+	void audio(ActionEvent event) {
+		String URL = "https:" + this.word.getAudio();
+		Media sound = new Media(URL);
+		MediaPlayer mediaPlayer = new MediaPlayer(sound);
+		mediaPlayer.play();
+	}
 
-  @FXML
-  void copyTarget(ActionEvent event) {
-    final Clipboard clipboard = Clipboard.getSystemClipboard();
-    final ClipboardContent content = new ClipboardContent();
-    content.putString(word.getTargetWord());
-    clipboard.setContent(content);
-  }
+	@FXML
+	void copySource(ActionEvent event) {
+		final Clipboard clipboard = Clipboard.getSystemClipboard();
+		final ClipboardContent content = new ClipboardContent();
+		content.putString(InputTextField.getText());
+		clipboard.setContent(content);
+	}
+
+	@FXML
+	void copyTarget(ActionEvent event) {
+		final Clipboard clipboard = Clipboard.getSystemClipboard();
+		final ClipboardContent content = new ClipboardContent();
+		content.putString(word.getTargetWord());
+		clipboard.setContent(content);
+	}
 
 
-  @FXML
-  void translate(ActionEvent event) {
-    Trans();
-    update(false);
-  }
+	@FXML
+	void translate(ActionEvent event) {
+		Trans();
+		update(false);
+	}
 
-  @FXML
-  void outputText(MouseEvent event) {
+	@FXML
+	void outputText(MouseEvent event) {
 
-  }
+	}
 
-  @FXML
-  void bookMark(ActionEvent event) {
-    int words = this.word.getSourceWord().split("\\s+").length;
-    if (words == 1) {
-      Database database = new Database();
-      database.createTable(this.word.getSourceWord(), this.word.getTargetWord(),
-          this.word.getString(), this.word.getAudio(), selectTargetBox.getValue());
-      database.close();
-      Alert alert = new Alert(AlertType.INFORMATION);
-      alert.setTitle("MESSAGE");
-      alert.setHeaderText(null);
-      alert.setContentText("The word has been added to the bookmark");
-      ((Stage) alert.getDialogPane().getScene().getWindow()).getIcons()
-          .add(new javafx.scene.image.Image("file:src/main/resources/assert/mess.png"));
-      alert.showAndWait();
-    } else {
-      Alert alert = new Alert(AlertType.ERROR);
-      alert.setTitle("ERROR");
-      alert.setHeaderText(null);
-      alert.setContentText("Please enter a single word");
-      ((Stage) alert.getDialogPane().getScene().getWindow()).getIcons()
-          .add(new javafx.scene.image.Image("file:src/main/resources/assert/error.png"));
-      alert.showAndWait();
-    }
-  }
+	@FXML
+	void bookMark(ActionEvent event) {
+		int words = this.word.getSourceWord().split("\\s+").length;
+		if (words == 1) {
+			Database database = new Database();
+			database.createTable(this.word.getSourceWord(), this.word.getTargetWord(),
+				this.word.getString(), this.word.getAudio(), selectTargetBox.getValue());
+			database.close();
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("MESSAGE");
+			alert.setHeaderText(null);
+			alert.setContentText("The word has been added to the bookmark");
+			((Stage) alert.getDialogPane().getScene().getWindow()).getIcons()
+				.add(new javafx.scene.image.Image("file:src/main/resources/assert/mess.png"));
+			alert.showAndWait();
+		} else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("ERROR");
+			alert.setHeaderText(null);
+			alert.setContentText("Please enter a single word");
+			((Stage) alert.getDialogPane().getScene().getWindow()).getIcons()
+				.add(new javafx.scene.image.Image("file:src/main/resources/assert/error.png"));
+			alert.showAndWait();
+		}
+	}
 
-  @FXML
-  void showBookMark(ActionEvent event) {
-    try {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../fxml/Bookmark.fxml"));
-      Parent root1 = loader.load();
-      Stage stage = new Stage();
-      Scene scene = new Scene(root1);
-      stage.setScene(scene);
-      stage.setTitle("Bookmark");
-      stage.setResizable(false);
-      stage.getIcons().add(new javafx.scene.image.Image("file:src/main/resources/assert/icon.png"));
-      stage.show();
-      showBMButton.setDisable(true);
-      stage.setOnCloseRequest((WindowEvent) -> {
-        showBMButton.setDisable(false);
-      });
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+	@FXML
+	void showBookMark(ActionEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../fxml/Bookmark.fxml"));
+			Parent root1 = loader.load();
+			Stage stage = new Stage();
+			Scene scene = new Scene(root1);
+			stage.setScene(scene);
+			stage.setTitle("Bookmark");
+			stage.setResizable(false);
+			stage.getIcons().add(new javafx.scene.image.Image("file:src/main/resources/assert/icon.png"));
+			stage.show();
+			showBMButton.setDisable(true);
+			stage.setOnCloseRequest((WindowEvent) -> {
+				showBMButton.setDisable(false);
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-  @FXML
-  void selectSource(ActionEvent event) {
-    this.sourceLang = languages.get(selectSourceBox.getValue());
-  }
+	@FXML
+	void selectSource(ActionEvent event) {
+		this.sourceLang = languages.get(selectSourceBox.getValue());
+	}
 
-  @FXML
-  void selectTarget(ActionEvent event) {
-    this.targetLang = languages.get(selectTargetBox.getValue());
-  }
+	@FXML
+	void selectTarget(ActionEvent event) {
+		this.targetLang = languages.get(selectTargetBox.getValue());
+	}
 
-  @FXML
-  void swap(ActionEvent event) {
-    this.sourceLang = languages.get(selectTargetBox.getValue());
-    this.targetLang = languages.get(selectSourceBox.getValue());
-    String temp = selectSourceBox.getValue();
-    selectSourceBox.setValue(selectTargetBox.getValue());
-    selectTargetBox.setValue(temp);
-  }
+	@FXML
+	void swap(ActionEvent event) {
+		this.sourceLang = languages.get(selectTargetBox.getValue());
+		this.targetLang = languages.get(selectSourceBox.getValue());
+		String temp = selectSourceBox.getValue();
+		selectSourceBox.setValue(selectTargetBox.getValue());
+		selectTargetBox.setValue(temp);
+	}
 
-  @FXML
-  void textListView(ActionEvent event) {
+	@FXML
+	void textListView(ActionEvent event) {
 
-  }
+	}
 
-  @FXML
-  void undo(ActionEvent event) {
-    if (history.size() > 0) {
-      future.push(this.word);
-      this.word = history.pop();
-      update(true);
-    }
-  }
+	@FXML
+	void undo(ActionEvent event) {
+		if (history.size() > 0) {
+		future.push(this.word);
+		this.word = history.pop();
+		update(true);
+		}
+	}
 
-  @FXML
-  void redo(ActionEvent event) {
-    if (future.size() > 0) {
-      history.push(this.word);
-      this.word = future.pop();
-      update(true);
-    }
-  }
+	@FXML
+	void redo(ActionEvent event) {
+		if (future.size() > 0) {
+		history.push(this.word);
+		this.word = future.pop();
+		update(true);
+		}
+	}
 
-  @FXML
-  void about(ActionEvent event) {
-    try {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../fxml/About.fxml"));
-      Parent root1 = loader.load();
-      Stage stage = new Stage();
-      Scene scene = new Scene(root1);
-      stage.setScene(scene);
-      stage.setTitle("About");
-      stage.setResizable(false);
-      stage.getIcons().add(new javafx.scene.image.Image("file:src/main/resources/assert/icon.png"));
-      stage.show();
-      aboutButton.setDisable(true);
-      stage.setOnCloseRequest((WindowEvent) -> {
-        aboutButton.setDisable(false);
-      });
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+	@FXML
+	void about(ActionEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../fxml/About.fxml"));
+			Parent root1 = loader.load();
+			Stage stage = new Stage();
+			Scene scene = new Scene(root1);
+			stage.setScene(scene);
+			stage.setTitle("About");
+			stage.setResizable(false);
+			stage.getIcons().add(new javafx.scene.image.Image("file:src/main/resources/assert/icon.png"));
+			stage.show();
+			aboutButton.setDisable(true);
+			stage.setOnCloseRequest((WindowEvent) -> {
+				aboutButton.setDisable(false);
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-  void Trans() {
-    if (!word.getSourceWord().equals("null")) {
-      future.clear();
-      history.push(this.word);
-    }
-    String source = InputTextField.getText().trim();
-    Translate trans = new Translate(source, "", targetLang);
-    trans.translateWord();
-    this.word = trans.getWord();
-  }
+	void Trans() {
+		if (!word.getSourceWord().equals("null")) {
+		future.clear();
+		history.push(this.word);
+		}
+		String source = InputTextField.getText().trim();
+		Translate trans = new Translate(source, "", targetLang);
+		trans.translateWord();
+		this.word = trans.getWord();
+	}
 
-  void update(boolean flag) {
-    stringTextArea.clear();
-    OutputTextArea.clear();
-    longOutputTextArea.clear();
-    if (flag) {
-      InputTextField.setText(this.word.getSourceWord());
-    }
-    int words = this.word.getSourceWord().split("\\s+").length;
-    if (words == 1) {
-      longOutputTextArea.setDisable(true);
-      OutputTextArea.setText(this.word.getTargetWord());
-      stringTextArea.setText(this.word.getString());
-      if (!this.word.getAudio().equals("none")) {
-        audioButton.setDisable(false);
-      } else {
-        audioButton.setDisable(true);
-      }
-    } else {
-      audioButton.setDisable(true);
-      longOutputTextArea.setDisable(false);
-      longOutputTextArea.setText(this.word.getTargetWord());
-    }
+	void update(boolean flag) {
+		stringTextArea.clear();
+		OutputTextArea.clear();
+		longOutputTextArea.clear();
+		if (flag) {
+		InputTextField.setText(this.word.getSourceWord());
+		}
+		int words = this.word.getSourceWord().split("\\s+").length;
+		if (words == 1) {
+		longOutputTextArea.setDisable(true);
+		OutputTextArea.setText(this.word.getTargetWord());
+		stringTextArea.setText(this.word.getString());
+		if (!this.word.getAudio().equals("none")) {
+			audioButton.setDisable(false);
+		} else {
+			audioButton.setDisable(true);
+		}
+		} else {
+		audioButton.setDisable(true);
+		longOutputTextArea.setDisable(false);
+		longOutputTextArea.setText(this.word.getTargetWord());
+		}
 
-    if (history.size() > 0) {
-      undoButton.setDisable(false);
-    } else {
-      undoButton.setDisable(true);
-    }
+		if (history.size() > 0) {
+		undoButton.setDisable(false);
+		} else {
+		undoButton.setDisable(true);
+		}
 
-    if (future.size() > 0) {
-      redoButton.setDisable(false);
-    } else {
-      redoButton.setDisable(true);
-    }
-    String target = this.word.getTargetWord();
-    target = target.replaceAll("\\s+", "");
-    if (!target.equals("") && !target.equals("null")) {
-      bookMarkButton.setDisable(false);
-    } else {
-      bookMarkButton.setDisable(true);
-    }
-  }
+		if (future.size() > 0) {
+		redoButton.setDisable(false);
+		} else {
+		redoButton.setDisable(true);
+		}
+		String target = this.word.getTargetWord();
+		target = target.replaceAll("\\s+", "");
+		if (!target.equals("") && !target.equals("null")) {
+		bookMarkButton.setDisable(false);
+		} else {
+		bookMarkButton.setDisable(true);
+		}
+	}
 
-  @Override
-  public void initialize(URL arg0, ResourceBundle arg1) {
-    redoButton.setDisable(true);
-    undoButton.setDisable(true);
-    bookMarkButton.setDisable(true);
-    InputTextField.setOnKeyPressed(event -> {
-      if (event.isShiftDown()) {
-        if (event.getCode().equals(KeyCode.ENTER)) {
-          Trans();
-          update(false);
-          event.consume();
-        }
-      }
-    });
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		redoButton.setDisable(true);
+		undoButton.setDisable(true);
+		bookMarkButton.setDisable(true);
+		InputTextField.setOnKeyPressed(event -> {
+		if (event.isShiftDown()) {
+			if (event.getCode().equals(KeyCode.ENTER)) {
+			Trans();
+			update(false);
+			event.consume();
+			}
+		}
+		});
+		word = new Word();
+		audioButton.setDisable(true);
+		selectSourceBox.getItems().addAll(languages.keySet());
+		selectTargetBox.getItems().addAll(languages.keySet());
+		selectSourceBox.setValue("English");
+		selectTargetBox.setValue("Vietnamese");
+		sourceLang = languages.get(selectSourceBox.getValue());
+		targetLang = languages.get(selectTargetBox.getValue());
 
-    word = new Word();
-    audioButton.setDisable(true);
-    selectSourceBox.getItems().addAll(languages.keySet());
-    selectTargetBox.getItems().addAll(languages.keySet());
-    selectSourceBox.setValue("English");
-    selectTargetBox.setValue("Vietnamese");
-    sourceLang = languages.get(selectSourceBox.getValue());
-    targetLang = languages.get(selectTargetBox.getValue());
-
-  }
+	}
 
 }
